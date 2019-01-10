35e3f2b8a061e6bdb3cf79ea52eb784e67fffc89
Andrey Antipov <Odrowonz@andreys-mac.local>
2019-01-10 23:43:54 +0300

    Initial Commit

---
+++ FindAndLight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FindAndLight
+//
+//  Created by Andrey Antipov on 10.Jan.19.
+//  Copyright © 2019 Andrey Antipov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
